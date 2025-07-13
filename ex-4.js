3b8023e23ebc37fb41ba745cfa2b8556af6da859
Jin <jinny2541@gmail.com>
2025-07-13 19:11:32 +0700

    ex1-6

--- ex-4.js
+++ ex-4.js
@@ -1,2 +1,17 @@
 // Start coding here
-let calculator = {};
+let add = (a, b) => a + b
+
+let subtract = (a, b) => a - b
+
+let multiply = (a, b) => a * b
+
+let divide = (a, b) => a / b
+
+
+let calculator = {add, subtract, multiply, divide};
+
+let addResult = calculator.add(3000, 10)
+let divideResult = calculator.divide(3000, 10)
+
+console.log(addResult)
+console.log(divideResult)
